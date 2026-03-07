110c9e5cd4a1fee419b7d7d5e4484edebdb5acd0
Pasta <mhavgonzales@gmail.com>
2026-03-07 16:33:25 +0200

    script code removed

--- behavior_pack/scripts/main.js
+++ behavior_pack/scripts/main.js
@@ -1,7 +1,1 @@
-﻿import { world } from "@minecraft/server";
-
-const LOG_PREFIX = "[WonderCraft Add-on]";
-
-world.events.tick.subscribe(() => {
-  console.debug(`${LOG_PREFIX} tick event`);
-});
+﻿
